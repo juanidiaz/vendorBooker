d5f01b9e103c09587405e6596d59625c03eaf634
Juan Diaz <jdi@idiaz.ca>
2019-04-09 19:59:57 -0400

    Adding folder structure from activity 20-11

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -10,15 +12,13 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+// Add routes, both API and view
+app.use(routes);
 
-// Define API routes here
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
