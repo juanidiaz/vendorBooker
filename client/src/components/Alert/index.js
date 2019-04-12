c4a147d36fcca40b613f98d5743b117313cb436f
Budhi Cahyadi <pcahyadi@gmail.com>
2019-04-12 13:03:40 -0400

    i added components, pages, utils

---
+++ client/src/components/Alert/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Alert(props) {
+  return (
+    <div
+      role="alert"
+      className={`alert alert-${props.type} fade in`}
+      style={{ width: "80%", margin: "0 auto", marginTop: 18, ...props.style }}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+export default Alert;
---
+++ client/src/components/Hero/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Hero(props) {
+  return (
+    <div className="hero text-center" style={{ backgroundImage: `url(${props.backgroundImage})` }}>
+      {props.children}
+    </div>
+  );
+}
+
+export default Hero;
