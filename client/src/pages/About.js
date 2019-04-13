fe686d039bf49682e03d0533fa70fb79104b214d
Budhi Cahyadi <pcahyadi@gmail.com>
2019-04-13 10:44:16 -0400

    I removed a paragraph

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -32,20 +32,6 @@ function About() {
               Aliquam blandit mi eu urna scelerisque, vitae volutpat ligula ultricies. Maecenas vel
               porta augue. Fusce mauris ex, dignissim et lacinia ut, tempus eget nibh.
             </p>
-            <p>
-              Etiam ut massa efficitur, gravida sapien non, condimentum sapien. Suspendisse massa
-              tortor, facilisis in neque sit amet, scelerisque elementum tortor. Nullam eget nibh
-              sit amet odio lobortis ullamcorper. Nulla bibendum magna nec sem pulvinar lobortis.
-              Mauris et imperdiet urna, vitae lobortis dui. Nunc elementum elit mi, non mattis enim
-              congue at. Proin mi lectus, ullamcorper id hendrerit eu, ultricies vitae lacus. Nunc
-              vehicula, erat eget laoreet condimentum, felis ante malesuada leo, nec efficitur diam
-              nisi eget nisi. Cras arcu lacus, tristique in bibendum vitae, elementum eget lorem.
-              Maecenas vestibulum volutpat orci eu pharetra. Praesent vel blandit ante, nec faucibus
-              libero. Sed ultrices lorem ex, eu facilisis libero convallis ac. Vivamus id dapibus
-              eros. Nullam tempor sem rhoncus porta semper. Proin bibendum vulputate nisl, fringilla
-              interdum elit pulvinar eu. Quisque vitae quam dapibus, vestibulum mauris quis, laoreet
-              massa.
-            </p>
           </Col>
         </Row>
       </Container>
