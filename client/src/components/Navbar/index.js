f65f156b7809353f2980518d2b87fccc09c889be
Budhi Cahyadi <pcahyadi@gmail.com>
2019-04-13 10:41:56 -0400

    I added a background picture

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -7,7 +7,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
-        Pupster
+        PetGrooming
       </Link>
       <div>
         <ul className="navbar-nav">
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -7,7 +7,7 @@ import Col from "../components/Col";
 function About() {
   return (
     <div>
-      <Background backgroundImage="https://images.search.yahoo.com/images/view;_ylt=AwrExo9C1rFcGkYA8ICJzbkF;_ylu=X3oDMTI0ZThkNmt2BHNlYwNzcgRzbGsDaW1nBG9pZAM2N2IyNzU3NDQwODZjODlmNmVmMDczNDFkOTdkNmQ3ZgRncG9zAzEwMARpdANiaW5n?back=https%3A%2F%2Fimages.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dcats%2Band%2Bdogs%2Bgrooming%26fr%3Dmcafee%26nost%3D1%26imgurl%3Dhttps%253A%252F%252Fsites.create-cdn.net%252Fsiteimages%252F35%252F5%252F2%252F355249%252F9718225.jpg%253F1425544365%26tab%3Dorganic%26ri%3D100&w=3258&h=1509&imgurl=www.safepet.com.au%2Fwp-content%2Fuploads%2F2015%2F06%2FDogs-and-Cats-in-a-Row-Large.jpg&rurl=http%3A%2F%2Fwww.safepet.com.au%2Fdog-grooming%2F&size=3385.7KB&name=Dog+grooming+%7C+Safe+Pet&p=cats+and+dogs+grooming&oid=67b275744086c89f6ef07341d97d6d7f&fr2=&fr=mcafee&tt=Dog+grooming+%7C+Safe+Pet&b=61&ni=192&no=100&ts=&tab=organic&sigr=1171btirv&sigb=16iob2h9k&sigi=12ecq6ock&sigt=10nmrhccv&sign=10nmrhccv&.crumb=aJ5ARDWm2wV&fr=mcafee">
+      <Background backgroundImage="http://amazingpetgrooming.ca/wp-content/uploads/2016/11/perro-secandose.jpg">
         <h1>Amazing Pet Grooming</h1>
         <h2>We are the best in Canada!</h2>
       </Background>
