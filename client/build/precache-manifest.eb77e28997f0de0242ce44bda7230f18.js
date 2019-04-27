6470a45d279b179e4210c81bc154f043a6967a99
Alex Yepes <alexisyepes@yahoo.com>
2019-04-26 22:10:08 -0400

    Ran build

---
+++ client/build/precache-manifest.eb77e28997f0de0242ce44bda7230f18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "94ecbd856ead68ffd362",
+    "url": "/static/js/main.5472977f.chunk.js"
+  },
+  {
+    "revision": "b5e8999b3472bdfe1959",
+    "url": "/static/js/2.727c05e5.chunk.js"
+  },
+  {
+    "revision": "94ecbd856ead68ffd362",
+    "url": "/static/css/main.53ae97db.chunk.css"
+  },
+  {
+    "revision": "b5e8999b3472bdfe1959",
+    "url": "/static/css/2.59798f76.chunk.css"
+  },
+  {
+    "revision": "e8997ec48cd712520a12302106e768c0",
+    "url": "/index.html"
+  }
+];
