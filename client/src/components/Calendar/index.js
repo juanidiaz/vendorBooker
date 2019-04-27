1828256500baf8ac5bfe2b02185a75f1cf961666
christopherdonner <christopher.donner@gmail.com>
2019-04-27 10:15:33 -0400

    re-adding Christopher's booking stuff (does not include Alex's code for protecting /booking)

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -2,18 +2,21 @@ import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 
-import '../../node_modules/fullcalendar/dist/fullcalendar.css'
+import '../../../node_modules/fullcalendar/dist/fullcalendar.css'
 
-import './index.css'
+import './style.css'
 
+// export default (props) => {
 function calendar(props){
-    return (
-      <FullCalendar
-      theme="bootstrap3"
-      default-view="month"
-      plugins={[ dayGridPlugin ]}
-      events={props.events}
-
+console.log(props.events)
+  return (
+    <FullCalendar
+    // {...props}
+    theme="bootstrap3"
+    default-view="month"
+    plugins={[ dayGridPlugin ]}
+    events=
+    {props.events}
       />
     )
   }
